6508008e938acc9603ba97d514c420962038c3eb
DamnVini <vinicius_v_jesus@estudante.sc.senai.br>
2019-09-14 19:11:10 -0300

    Página do conversor finalizada e adicionado uma página para as fórmulas

--- js/conversor.js
+++ js/conversor.js
@@ -1,5 +1,10 @@
-// Function que pega o campo selecionado e converte
+    
+    var div = document.getElementById('alert');
+    var HTMLDivId = "\"animated-example\"";
+    var HTMLDivClass =  "\"w3-display-bottommiddle w3-panel w3-red animated fadeOut\"";
+    var HTMLshake = "\"shake\"";
 
+// Function que pega o campo selecionado e converte
 function conversor(Input) {
 
     var fObj = document.convert.ftemp;
@@ -11,6 +16,7 @@ function conversor(Input) {
 
         cObj.value = ((fObj.value - 32) * (5 / 9)).toFixed(2);
         kObj.value = (((fObj.value - 32) * (5 / 9)) + (273.15)).toFixed(2);
+        Verificar();
 
     }
     else if (Input == "kelvin") 
@@ -18,23 +24,52 @@ function conversor(Input) {
 
         fObj.value = ((kObj.value - 273.15) * (9 / 5) + 32).toFixed(2); 
         cObj.value = ((fObj.value - 32) * (5 / 9)).toFixed(2);
+        Verificar();
 
     }
     else if (Input == "celsius")
     {
 
-        fObj.value = ((parseInt(cObj.value) * (9 / 5)) + 32).toFixed(2);
-        kObj.value = ((parseInt(cObj.value) + 273)).toFixed(2);
+        fObj.value = ((cObj.value * (9 / 5)) + 32).toFixed(2);
+        kObj.value = ((parseInt(cObj.value) + 273.15)).toFixed(2);
+        Verificar();
 
     }
+
 }
 
 // Limpa todos os campos
-function clearAll() 
+function Limpar() 
 {
+    
     document.convert.ftemp.value = "";
     document.convert.ctemp.value = "";
     document.convert.ktemp.value = "";
 }
+
+function KelvinNegativo() 
+{
+    var OnScreen;
+    document.getElementById("alert").innerHTML += 
+    "<div id=" + HTMLDivId + " class=" + HTMLDivClass + "> <h3 class=" + HTMLshake + ">ERRO!</h3> <p class=" + HTMLshake + ">Kelvin NÃO pode ser negativo!</p></div>";
+}
+
+function Verificar() 
+{
+    if(document.convert.ktemp.value < 0)
+    {
+        KelvinNegativo();
+        Limpar();
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
